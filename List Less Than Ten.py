9a90abc9c777af191575ed68f1bd572dfa0f42cb
Jimasd <37746668+Jimasd@users.noreply.github.com>
2018-03-24 18:12:42 -0400

    Update List Less Than Ten.py

--- List Less Than Ten.py
+++ List Less Than Ten.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 21 15:52:31 2018
-
-@author: Jimmy
-"""
-
 #creer une liste qui retourne les nombres en bas de x
 nb=int(input("En bas de quel nombre?:"))
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
